2707f9f31b8a7c09da0cb37a8e3ac2b89c578499
sushi-chaaaan <mail@sushichan.live>
2024-04-17 11:02:17 +0900

    fix: remove monorepo deps from web

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,8 @@
-import { getApi } from "@/lib/api"
-
-export default async function Home() {
-  const api = getApi()
-  const res = await (await api.hello.$get()).text().catch((err) => String(err))
-
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {res}
+        haha
       </div>
     </main>
   )
