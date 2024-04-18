b651278c7c3155eb0b91fb40ae9637d84f615cfc
sushi-chaaaan <mail@sushichan.live>
2024-04-18 16:44:56 +0900

    feat: add turso db

---
+++ packages/api/src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/libsql"
+import { createClient } from "@libsql/client"
+
+import schema from "./schemas"
+
+const createDB = (...arg: Parameters<typeof createClient>) => {
+  const client = createClient(...arg)
+  const db = drizzle(client, { schema })
+  return db
+}
+
+export { createDB }
