9eaf615013982baf43b3c5a8e5d0c34dca74297e
sushi-chaaaan <mail@sushichan.live>
2024-04-20 21:51:07 +0900

    fix: use env var to switch api client behavior

--- apps/frontend/lib/api.ts
+++ apps/frontend/lib/api.ts
@@ -2,15 +2,15 @@ import apiClientFactory from "@repo/api/client"
 import { AppLoadContext } from "@remix-run/cloudflare"
 
 const getApi = ({ context: ctx }: { context: AppLoadContext }) => {
-  const { BACKEND } = ctx.cloudflare.env
+  const { BACKEND, API_MODE } = ctx.cloudflare.env
   // we need to set some hostname, even if used with Service Bindings at production
   // https://community.cloudflare.com/t/service-binding-to-other-worker-not-working/559030/5
   const basePath = "http://localhost:8787"
   //@ts-expect-error type of 1st-arg does not match...
-  const fetchOptions: Parameters<typeof apiClientFactory>["1"] = import.meta.env
-    .PROD
-    ? { fetch: BACKEND.fetch.bind(BACKEND) }
-    : undefined
+  const fetchOptions: Parameters<typeof apiClientFactory>["1"] =
+    API_MODE === "production"
+      ? { fetch: BACKEND.fetch.bind(BACKEND) }
+      : undefined
 
   return apiClientFactory(basePath, fetchOptions)
 }
