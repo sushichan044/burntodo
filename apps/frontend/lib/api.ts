35cd459c6b26ce94a10a90efc367d338fca5eb09
sushi-chaaaan <mail@sushichan.live>
2024-04-20 22:12:01 +0900

    なんもわからん

--- apps/frontend/lib/api.ts
+++ apps/frontend/lib/api.ts
@@ -5,7 +5,7 @@ const getApi = ({ context: ctx }: { context: AppLoadContext }) => {
   const { BACKEND, API_MODE } = ctx.cloudflare.env
   // we need to set some hostname, even if used with Service Bindings at production
   // https://community.cloudflare.com/t/service-binding-to-other-worker-not-working/559030/5
-  const basePath = "http://localhost:8787"
+  const basePath = API_MODE === "production" ? "/" : "http://localhost:8787"
   //@ts-expect-error type of 1st-arg does not match...
   const fetchOptions: Parameters<typeof apiClientFactory>["1"] =
     API_MODE === "production"
