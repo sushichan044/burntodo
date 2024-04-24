a2a119199b9bd17c1559e89386e25255266060bb
sushi-chaaaan <mail@sushichan.live>
2024-04-25 02:29:44 +0900

    chore: update packages

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,4 +1,10 @@
 import react from "@virtual-live-lab/eslint-config/presets/react"
 import tailwind from "@virtual-live-lab/eslint-config/addons/tailwind"
 
-export default [...react, ...tailwind]
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(...react, ...tailwind, {
+  languageOptions: {
+    parser: tseslint.parser,
+  },
+})
