1f20dff399bcf25255c178fa02342ab1a62cdc9a
sushi-chaaaan <mail@sushichan.live>
2024-05-02 03:17:04 +0900

    fix: cookie option

--- apps/frontend/app/sessions.server.ts
+++ apps/frontend/app/sessions.server.ts
@@ -9,20 +9,32 @@ type SessionFlashData = {
   error: string
 }
 
-const { commitSession, destroySession, getSession } =
-  createCookieSessionStorage<SessionData, SessionFlashData>({
-    // a Cookie from `createCookie` or the CookieOptions to create one
-    cookie: {
-      expires: new Date(Date.now() + 60_000),
-      httpOnly: true,
-      // Expires can also be set (although maxAge overrides it when used in combination).
-      // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
-      //
-      name: "__session",
-      path: "/",
-      sameSite: "lax",
-      secure: true,
-    },
+const {
+  commitSession: rawCommitSession,
+  destroySession,
+  getSession,
+} = createCookieSessionStorage<SessionData, SessionFlashData>({
+  // a Cookie from `createCookie` or the CookieOptions to create one
+  cookie: {
+    httpOnly: true,
+    // Expires can also be set (although maxAge overrides it when used in combination).
+    // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
+    //
+    name: "__session",
+    path: "/",
+    sameSite: "lax",
+    secrets: ["hello!", "This is sample secret key"],
+    secure: true,
+  },
+})
+
+type CommitSessionFunction = typeof rawCommitSession
+
+const commitSession: CommitSessionFunction = (session, options) => {
+  return rawCommitSession(session, {
+    expires: new Date(Date.now() + 60_000),
+    ...options,
   })
+}
 
 export { commitSession, destroySession, getSession }
