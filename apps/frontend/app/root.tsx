d11bbb3231c4bcca690e8775ec4861bd374126ef
sushi-chaaaan <mail@sushichan.live>
2024-05-02 14:54:58 +0900

    feat: add progress bar

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -1,16 +1,30 @@
 import { ColorSchemeScript, MantineProvider } from "@mantine/core"
 import "@mantine/core/styles.css"
+import { NavigationProgress, nprogress } from "@mantine/nprogress"
+import "@mantine/nprogress/styles.css"
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigation,
 } from "@remix-run/react"
+import { useLayoutEffect } from "react"
 
 import "./style.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const navigation = useNavigation()
+  useLayoutEffect(() => {
+    if (navigation.state === "loading") {
+      nprogress.start()
+    }
+    if (navigation.state === "idle") {
+      nprogress.complete()
+    }
+  }, [navigation.state])
+
   return (
     <html lang="ja">
       <head>
@@ -21,7 +35,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <NavigationProgress color="orange" size={2} />
+          {children}
+        </MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
