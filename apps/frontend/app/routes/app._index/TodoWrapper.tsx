86b0b1e4e65e9ff5675fa013571a5773c786f63e
sushi-chaaaan <mail@sushichan.live>
2024-05-04 04:21:49 +0900

    fix: remove framer motion

--- apps/frontend/app/routes/app._index/TodoWrapper.tsx
+++ apps/frontend/app/routes/app._index/TodoWrapper.tsx
@@ -1,8 +1,6 @@
 import type { TB_TodoSelect } from "@repo/module/zod"
 
 import Todo from "@/app/routes/app._index/Todo"
-import { AnimatePresence } from "framer-motion"
-import { LazyMotion, domAnimation } from "framer-motion"
 
 type TodoWrapperProps = {
   todos: Pick<TB_TodoSelect, "description" | "id" | "title">[]
@@ -10,15 +8,11 @@ type TodoWrapperProps = {
 
 const TodoWrapper: React.FC<TodoWrapperProps> = ({ todos }) => {
   return (
-    <LazyMotion features={domAnimation} strict>
-      <AnimatePresence mode="wait">
-        <div className="flex max-h-[80vh] flex-col gap-y-1.5 overflow-y-auto scrollbar scrollbar-thumb-slate-300 scrollbar-thumb-rounded-full scrollbar-w-2 md:gap-y-3">
-          {todos.map((task, i) => {
-            return <Todo key={i} task={task} />
-          })}
-        </div>
-      </AnimatePresence>
-    </LazyMotion>
+    <div className="flex max-h-[80vh] flex-col gap-y-1.5 overflow-y-auto scrollbar scrollbar-thumb-slate-300 scrollbar-thumb-rounded-full scrollbar-w-2 md:gap-y-3">
+      {todos.map((task, i) => {
+        return <Todo key={i} task={task} />
+      })}
+    </div>
   )
 }
 
