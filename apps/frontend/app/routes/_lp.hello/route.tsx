e3e95e0aed12a53430e3d4f3fdbd8feadc068bf6
sushi-chaaaan <mail@sushichan.live>
2024-05-06 14:37:24 +0900

    feat: add hello route

---
+++ apps/frontend/app/routes/_lp.hello/route.tsx
@@ -0,0 +1,29 @@
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare"
+
+import { getApi } from "@/lib/api"
+import { Container } from "@mantine/core"
+import { useLoaderData } from "@remix-run/react"
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const api = getApi({ context })
+  return await api.hello
+    .$get()
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error(err)
+      return { error: "Failed to fetch data", message: null }
+    })
+}
+
+export default function Route() {
+  const data = useLoaderData<typeof loader>()
+
+  return (
+    <Container className="py-4" size="md">
+      <h1 className="text-3xl font-bold">Response from backend</h1>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </Container>
+  )
+}
