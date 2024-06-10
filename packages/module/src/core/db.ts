935f59aeb25e60c8494d08952db81737b214f5b3
sushi-chaaaan <mail@sushichan.live>
2024-06-11 02:25:44 +0900

    try prisma

--- packages/module/src/core/db.ts
+++ packages/module/src/core/db.ts
@@ -1,8 +1,16 @@
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
 import * as schema from "@repo/module/schema";
 import { drizzle } from "drizzle-orm/d1";
 
 const createDB = (db: D1Database) => drizzle(db, { schema });
 type DBType = ReturnType<typeof createDB>;
 
-export { createDB };
-export type { DBType };
+const createPrisma = (db: D1Database) => {
+  const adapter = new PrismaD1(db);
+  return new PrismaClient({ adapter });
+};
+type PrismaType = ReturnType<typeof createPrisma>;
+
+export { createDB, createPrisma };
+export type { DBType, PrismaType };
