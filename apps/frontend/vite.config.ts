eb2e6b05038c13d33bf1b889d502504b71b5338a
sushichan044 <mail@sushichan.live>
2024-12-14 19:34:39 +0900

    enable v3_fetcherPersist

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [
     // 改行対策
     remixCloudflareDevProxy(),
-    remix({ future: { v3_singleFetch: true } }),
+    remix({ future: { v3_fetcherPersist: true, v3_singleFetch: true } }),
     babel({
       babelConfig: {
         plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
