a69a4ed88b6336c0c0e5436de40a8765d77e2496
sushichan044 <mail@sushichan.live>
2024-12-14 19:39:00 +0900

    enable v3_relativeSplatPath

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       future: {
         v3_fetcherPersist: true,
         v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
         v3_singleFetch: true,
         v3_throwAbortReason: true,
       },
