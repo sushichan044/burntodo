3f98842d569d26c925c504a0242c3248255d259d
sushichan044 <mail@sushichan.live>
2024-12-14 19:20:29 +0900

    ignore err for remix things

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import tailwind from "@virtual-live-lab/eslint-config/addons/tailwind";
 import react from "@virtual-live-lab/eslint-config/presets/react";
 import reactCompiler from "eslint-plugin-react-compiler";
+import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(...react, ...tailwind, {
@@ -9,8 +10,13 @@ export default tseslint.config(...react, ...tailwind, {
   },
   plugins: {
     "react-compiler": reactCompiler,
+    "react-refresh": reactRefresh,
   },
   rules: {
     "react-compiler/react-compiler": 2,
+    "react-refresh/only-export-components": [
+      "error",
+      { allowExportNames: ["meta", "links", "headers", "loader", "action"] },
+    ],
   },
 });
